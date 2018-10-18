cc5d6103ea7e6b3215b65e7987fd16041aa688c9
vitalii Sili <rolisangor@github.com>
2018-10-18 18:03:01 +0300

    Простые табы

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+ function simpleTabs() {
+    let tab = document.querySelectorAll('.info-header-tab'),  //  <==  Название класса табов
+        info = document.querySelector('.info-header'),   //  <==  Название класса родителя  для табов  или ID
+        tabContent = document.querySelectorAll('.info-tabcontent'); //  <==  Название классов для блока информации
+
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+}
+simpleTabs();
